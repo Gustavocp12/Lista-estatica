9b34a3d7f1c10d469c215358c163f8d0a711ebec
Gustavocp12 <82041683+Gustavocp12@users.noreply.github.com>
2022-03-12 10:13:11 -0300

    Add files via upload

---
+++ ListaEstatica/lista.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaEstatica
+{
+    class lista
+    {
+
+        string[] valores;
+        int limite, inicio, final;
+
+        public lista(int tamanho)
+        {
+
+            valores = new string[tamanho];
+            limite = tamanho - 1;
+            inicio = -1;
+            final = -1;
+
+        }
+        
+        public void inserir(string valor)
+        {
+
+            if (final < limite)
+            {
+
+                final++;
+                valores[final] = valor;
+
+            }
+            else
+            {
+
+                Console.WriteLine("Lista cheia!");
+
+            }
+
+        }
+
+        public void inserir(string valor, int posicao)
+        {
+
+            if (final < limite)
+            {
+
+                if ((posicao >= 0) && (posicao <= limite))
+                {
+
+                    final++;
+
+                    for (int i = 0; i < (final - posicao); i++)
+                    {
+
+                        valores[(final + 1) - i] = valores[final - i];
+
+                    }
+
+                    valores[posicao] = valor;
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Posição fora da lista!");
+
+                }
+
+            }
+            else
+            {
+
+                Console.WriteLine("A lista está cheia!");
+
+            }
+
+        }
+
+        public void imprimir()
+        {
+
+            for (int i = 0; i <= final; i++)
+            {
+
+                Console.WriteLine(valores[i]);
+
+            }
+
+
+        }
+
+
+    }
+}
